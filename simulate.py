a4038f1e1e8efcebc3f8c5ab36523dac162814e3
Ronniy Joseph <ronniy.joseph@icrar.org>
2021-11-09 10:56:37 -0500

    Built class to classes in generate

--- simulate.py
+++ simulate.py
@@ -3,3 +3,18 @@
 Defines a class and relevant functions that provides an interface
 to setup up a simulation pass along user inputs etc.
 """
+
+from generate import VisibilityEngine
+
+class Simulator:
+
+    def __init__(self):
+        self.array_layout = "inputs/array_layout.txt"
+        self.telescope_md = "inputs/telescope_metadata.yaml"
+        self.simulation_settings = "inputs/settings.yaml"
+        return
+
+    def run(self):
+        visibilities = VisibilityEngine(settings_file = self.simulation_settings)
+        visibilities.generate()
+        return
