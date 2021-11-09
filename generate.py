81ee5d8551107ad87b801e6e43fd98db13943499
Ronniy Joseph <ronniy.joseph@icrar.org>
2021-11-09 10:56:11 -0500

    Built class to call pyvusim

--- generate.py
+++ generate.py
@@ -3,3 +3,21 @@
 Defines a class and relevant functions that sets up the visibility creation
 through pyuvsim, and takes care of all ways we want to peturb the visibilities
 """
+
+import pyuvsim
+
+class VisibilityEngine:
+
+    def __init__(self, settings_file):
+        self.settings = settings_file
+        return
+
+    def generate(self):
+        pyuvsim.uvsim.run_uvsim(self.settings, return_uv=True)
+        return
+
+    def apply_gains(self):
+        return
+
+    def add_noise(self):
+        return
