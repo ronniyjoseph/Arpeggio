cb2897b0f10bb528ca5b566179f5927cefe56de7
Ronniy Joseph <ronniy.joseph@mcgill.ca>
2021-11-10 15:35:42 -0500

    Fixed Kelvin to Jansky Conversion

--- arpeggio/perturbate.py
+++ arpeggio/perturbate.py
@@ -9,37 +9,38 @@ from scipy.constants import Boltzmann as k_B
 
 
 class ThermalNoise:
-    def __init__(self, uvdata=None, T_sys=30, channel_width=40e3, t_integrate=120, A_eff=4**2):
-        #TODO figure out from pyvudata what
-        #frequency dependence
-        #Or take input
-        if uvdata != None:
-            # Figure things out from pyuvdata object
-            self.t_integrate = uvdata.integration_time
-        else:
-        #user defined path
-            self.t_integrate = t_integrate[0]
 
-        #parameters that are not in pyuvobject or not always correct
-        #single frequency channel tends to have wrong channel width
+    def __init__(self, uvdata=None, T_sys=30, channel_width=390e3, t_integrate=10, A_eff=np.pi*6**2, freqs=1e9):
+        # Initialize ThermalNoise object with user defined parameters
+        #TODO use pyvudata to fill in some parameters?
+        #TODO add in astropy units??
+        #TODO add frequency dependent Tsys
+        #TODO add frequency dependent Aeff/option to compute Aeff from beam sims
+
+        self.t_integrate = t_integrate
         self.T_sys = T_sys
         self.channel_width = channel_width
         self.A_eff = A_eff
+        self.freqs =  freqs
         return
 
-    def noise_temperature(self):
-        Tnoise = self.T_sys /(np.sqrt(self.channel_width * self.t_integrate))
-        return Tnoise
+    def temperature(self):
+        #computes noise in temperature units [K]
+        T_noise = self.T_sys /(np.sqrt(self.channel_width * self.t_integrate))
+        return T_noise
 
-    def add_noise(self, uvdata):
-        if self.shape == None:
-            pass
-        else:
-            noise_jy =  2.*k_B*self.noise_temperature(self)/self.A_eff
-            noise_realisation = np.random.normal(scale=noise_jy, shape=self.shape) + \
-                                1j*np.random.normal(scale=noise_jy, shape=self.shape)
-            uvdata.data_array += noise_realisation
-        return
+    def visibility(self):
+        #computes noise in visibility units [Jy]
+        v_noise = np.sqrt(2.)*k_B*self.temperature()/self.A_eff*1e26
+        return v_noise
+
+    def apply(self, uvdata):
+        shape = uvdata.data_array.shape
+        noise_jy = self.visibility()
+        noise_realisation = np.random.normal(scale=noise_jy, size=shape) + \
+                            1j*np.random.normal(scale=noise_jy, size=shape)
+        uvdata.data_array += noise_realisation
+        return uvdata
 
 class AntennaGains:
     def __init__(self):
